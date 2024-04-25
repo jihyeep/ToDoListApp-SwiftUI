0afc6cc2ac967f1c0f68c3b2ee5c1a472fa507e3
Jihye Park <deviloper.park@gmail.com>
2024-04-25 21:37:18 +0900

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by 박지혜 on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
